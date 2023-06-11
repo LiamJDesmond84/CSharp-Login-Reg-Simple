596ecb1af4b08561cf69f4dfb1bb6d5fe5348416
Liam James Desmond <liamjdesmond84@gmail.com>
2023-06-11 16:35:48 -0500

    Adding skeleton & gitattributes

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CSharp_Login_Reg_Simple.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
