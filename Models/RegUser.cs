2db51982e388f5b6a72e270ac66a245f28333d82
Liam James Desmond <liamjdesmond84@gmail.com>
2023-06-15 20:01:36 -0500

    Adding models

--- Models/RegUser.cs
+++ Models/RegUser.cs
@@ -1,6 +1,23 @@
-﻿namespace CSharp_Login_Reg_Simple.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CSharp_Login_Reg_Simple.Models
 {
     public class RegUser
     {
+        [Required]
+        [MinLength(1)]
+        private string FirstName;
+
+        [Required]
+        [MinLength(1)]
+        private string LastName;
+
+        [Required]
+        [MinLength(3)]
+        private string Password;
+
+        [EmailAddress]
+        private string Email;
+
     }
 }
