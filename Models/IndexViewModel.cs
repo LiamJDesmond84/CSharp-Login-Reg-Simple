f4b3edd3792f7dee37598d9672107bdb7a39e6fc
Liam James Desmond <liamjdesmond84@gmail.com>
2023-06-15 20:20:49 -0500

    Added IndexViewModel

---
+++ Models/IndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharp_Login_Reg_Simple.Models
+{
+    public class IndexViewModel
+    {
+        public RegUser? RegUser { get; set; }
+
+        public LogUser? LogUser { get; set; }
+
+    }
+}
