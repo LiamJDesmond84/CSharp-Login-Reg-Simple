93dce4597e63e6c5343a34a1d5492daf583bf868
Liam James Desmond <liamjdesmond84@gmail.com>
2023-06-17 19:53:28 -0500

    Adding notes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -66,4 +66,17 @@ public class HomeController : Controller
 
         return View();
     }
+
+
+    //// *Inside controller methods*
+    //// To store a string in session we use ".SetString"
+    //// The first string passed is the key and the second is the value we want to retrieve later
+    //HttpContext.Session.SetString("UserName", "Samantha");
+    //// To retrieve a string from session we use ".GetString"
+    //string LocalVariable = HttpContext.Session.GetString("UserName");
+
+    //// To store an int in session we use ".SetInt32"
+    //HttpContext.Session.SetInt32("UserAge", 28);
+    //// To retrieve an int from session we use ".GetInt32"
+    //int? IntVariable = HttpContext.Session.GetInt32("UserAge");
 }
