12b4cdf7d93b716b8618a530761e2ddd7491ce80
Liam James Desmond <liamjdesmond84@gmail.com>
2023-06-17 19:55:09 -0500

    Adding notes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -71,12 +71,16 @@ public class HomeController : Controller
     //// *Inside controller methods*
     //// To store a string in session we use ".SetString"
     //// The first string passed is the key and the second is the value we want to retrieve later
-    //HttpContext.Session.SetString("UserName", "Samantha");
+    //      HttpContext.Session.SetString("UserName", "Samantha");
     //// To retrieve a string from session we use ".GetString"
-    //string LocalVariable = HttpContext.Session.GetString("UserName");
+    //      string LocalVariable = HttpContext.Session.GetString("UserName");
 
     //// To store an int in session we use ".SetInt32"
-    //HttpContext.Session.SetInt32("UserAge", 28);
+    //      HttpContext.Session.SetInt32("UserAge", 28);
     //// To retrieve an int from session we use ".GetInt32"
-    //int? IntVariable = HttpContext.Session.GetInt32("UserAge");
+    //      int? IntVariable = HttpContext.Session.GetInt32("UserAge");
+
+    // We can clear our session with the Clear method:
+
+    // HttpContext.Session.Clear();
 }
